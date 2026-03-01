aebd193a705a35b88665615e237bee20e07318c7
hipika <arnavchoudhary53@gmail.com>
2026-03-01 16:12:55 -0500

    formatted dashboard page

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,57 +1,40 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import styled from 'styled-components'
-import Link from 'next/link'
-import Navbar from '@/components/Dashboard/Navbar'
 import { useStateContext } from '@/context/StateContext'
 import { useRouter } from 'next/router'
-
+import DashboardNavbar from '@/components/Dashboard/DashboardNavbar'
 
 const Dashboard = () => {
-
-  const { user } = useStateContext()  
-
+  const { user } = useStateContext()
   const router = useRouter()
 
-
   useEffect(() => {
-    if(!user){
+    if (!user) {
       router.push('/')
-    }else{
-
     }
-  }, user)
-
-
-
+  }, [user])
 
   return (
-    <Section>
-      {/* <TopHeader>
-        Dashboard
-      </TopHeader> */}
-
-
-    </Section>
+    <>
+      <DashboardNavbar />
+      
+      <Section>
+        <TopHeader>Dashboard</TopHeader>
+      </Section>
+    </>
   )
 }
 
-
-//STYLED COMPONENTS
 const Section = styled.section`
-width: 100%;
-height: 100vh;
-display: flex;
-justify-content: center;
+  width: 100%;
+  height: 100vh;
+  padding-top: 2rem;
+  display: flex;
+  justify-content: center;
 `
-
-
 
 const TopHeader = styled.h1`
-font-size: 26px;
-display: flex;
-
+  font-size: 26px;
 `
-
-
 
 export default Dashboard
