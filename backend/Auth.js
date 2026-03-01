c73d83289749ef5385030c230441e8f741c17d70
hipika <arnavchoudhary53@gmail.com>
2026-03-01 15:10:38 -0500

    implemented auth.js

--- backend/Auth.js
+++ backend/Auth.js
@@ -1,3 +1,25 @@
 import { auth } from "./Firebase";
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, fetchSignInMethodsForEmail } from "firebase/auth";
-  
+
+export async function register(email, password, setUser) {
+  const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+  const user = userCredential.user;
+  setUser(user);
+  console.log("User registered:", user);
+  return user;
+}
+
+
+export async function login(email, password, setUser) {
+  const userCredential = await signInWithEmailAndPassword(auth, email, password);
+  const user = userCredential.user;
+  setUser(user);
+  console.log("User signed in:", user);
+  return user;
+}
+
+
+export async function isEmailInUse(email) {
+  return fetchSignInMethodsForEmail(auth, email);
+}
+
