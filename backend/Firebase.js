b8f07768682ef6f39f8e931a1893a054295d62c7
hipika <arnavchoudhary53@gmail.com>
2026-03-02 01:05:21 -0500

    final

--- backend/Firebase.js
+++ backend/Firebase.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
@@ -11,7 +10,7 @@ import { getAuth } from "firebase/auth";;
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAZCb0b6Mzstn6VGzSqQ5W8dCwyFXh09Ts",
+  apiKey: process.env.FIREBASE_API_KEY,
   authDomain: "travelify2.firebaseapp.com",
   projectId: "travelify2",
   storageBucket: "travelify2.firebasestorage.app",
@@ -33,37 +32,3 @@ const database = getFirestore(app);
 const storage = getStorage(app);
 
 export { app, auth, database, storage, analytics };
-=======
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
-import { getAuth } from "firebase/auth";
-
-const firebaseConfig = process.env.NODE_ENV === 'production' ? {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "", 
-  measurementId: ""
-} : {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "com",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
-}
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const storage = getStorage(app);
-export const database = getFirestore(app);
-export const analytics = () => getAnalytics(app);
-
-export default app
->>>>>>> 0e76531abd0b228ee570b4d2d2634a89fe2c7dd9
